f9393530a6e5342e45e8b23b02634d1329c16ae3
Ethan Youm <sy327@duke.edu>
2025-06-24 00:20:52 -0400

    Create ResNet18.py

---
+++ Models/ResNet18.py
@@ -0,0 +1,17 @@
+from torchvision.models import ResNet18_Weights
+
+# Define ResNet model
+model_resnet = models.resnet18(weights = ResNet18_Weights.IMAGENET1K_V1)
+num_classes = 10
+
+inputs_try , labels_try = inputs_try.to(device), labels_try.to(device)
+model_resnet = model_resnet.to(device)
+outputs_try = model_resnet(inputs_try)
+
+criterion = nn.CrossEntropyLoss()
+optimizer_resnet = optim.Adam(model_resnet.parameters(), lr = 0.001)
+
+resnet_train_losses, resnet_val_losses = train_model_with_loss_tracking(model_resnet, train_loader = train_loader,
+                                                                        val_loader = test_loader,
+                                                                        criterion = criterion, optimizer = optimizer_resnet,
+                                                                        epochs = epochs)
