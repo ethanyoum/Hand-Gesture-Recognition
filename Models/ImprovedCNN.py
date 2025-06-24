2f9749c571f2c53bb0492e4d10eaa02c7c1f154b
Ethan Youm <sy327@duke.edu>
2025-06-24 00:28:11 -0400

    Create ImprovedCNN.py

---
+++ Models/ImprovedCNN.py
@@ -0,0 +1,46 @@
+class ImprovedNetAgain(nn.Module):
+    def __init__(self):
+        super(ImprovedNetAgain, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(2, 2))
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3))
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3))
+        self.conv3_bn = nn.BatchNorm2d(64)
+        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * 6 * 6, 64)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1_bn(self.conv1(x))))
+        x = self.pool2(F.relu(self.conv2_bn(self.conv2(x))))
+        x = self.pool3(F.relu(self.conv3_bn(self.conv3(x))))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+model = ImprovedNetAgain().to(device)
+
+# Specify optimizer again
+optimizer_again = optim.Adam(model.parameters(), lr = 0.0001, weight_decay = 1e-4)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Train
+dset_sizes = {'trainval': len(train_dataset)}
+model_cnn_2, training_loss_2, training_acc_2 = train_model(model, train_loader, size=dset_sizes['trainval'], epochs=epochs, optimizer=optimizer_again)
+
+# Test
+dset_sizes = {'test': len(test_dataset)}
+num_classes = len(full_dataset.lookup)
+predictions_CNN, all_proba_CNN, all_classes_CNN, test_loss_CNN, test_acc_CNN = test_model(model,test_loader, size=dset_sizes['test'], criterion=criterion, 
+                                                                                          num_epochs = epochs,device=device)
